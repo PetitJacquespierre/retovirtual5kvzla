c3ff2344a9905d49986859757498b08497129a20
PetitJacquespierre <petitjacquespierre@gmail.com>
2026-05-07 01:52:43 -0400

    Add files via upload

--- api/gemini.js
+++ api/gemini.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                model: 'llama-3.1-70b-versatile', // Modelo potente y rápido
+                model: 'llama-3.3-70b-versatile', // Modelo potente y rápido
                 messages: [{
                     role: 'user',
                     content: prompt
